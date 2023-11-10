e5cb31434bf8b0e78698b5f0ad402ddfdc1168bc
khoadt9 <khoadt9@fpt.com>
2023-11-10 11:29:18 +0700

    up

---
+++ public/globalConfig.js
@@ -0,0 +1,5 @@
+window.globalConfig = {
+    APP_ENV:"production",
+    appId:1,
+    APP_DOMAIN: "http://localhost:9999",
+}
---
+++ src/utils/Constants.js
@@ -0,0 +1,7 @@
+
+export default {
+    API: {
+        GET_USER: '/access/user',
+        GET_TOKEN: '/access/get-token'
+    }
+}
